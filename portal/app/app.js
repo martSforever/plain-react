30ab414d5d1d944d68e3cff38589e9ac3bfb4b68
韦胜健 <martsforever@aliyun.com>
2019-05-20 21:01:04 +0800

    disabled eslint

--- portal/app/app.js
+++ portal/app/app.js
@@ -1,34 +1,137 @@
 import React from 'react';
 import AppMenu from './app-menu'
 
+class Dep {
+    constructor() {
+        this.deps = []
+    }
+
+    depend() {
+        if (Dep.target && this.deps.indexOf(Dep.target) === -1) {
+            this.deps.push(Dep.target)
+        }
+    }
+
+    notify() {
+        this.deps.forEach((dep) => {
+            dep()
+        })
+    }
+}
+
+Dep.target = null
+
+class Observable {
+    constructor(obj) {
+        return this.walk(obj)
+    }
+
+    walk(obj) {
+        const keys = Object.keys(obj)
+        keys.forEach((key) => {
+            this.defineReactive(obj, key, obj[key])
+        })
+        return obj
+    }
+
+    defineReactive(obj, key, val) {
+        const dep = new Dep()
+        Object.defineProperty(obj, key, {
+            get() {
+                dep.depend()
+                return val
+            },
+            set(newVal) {
+                val = newVal
+                dep.notify()
+            }
+        })
+    }
+}
+
+class Compute {
+
+    /**
+     * @author  韦胜健
+     * @date    2019/5/20 20:35
+     * @param   obj                 计算属性所属的对象
+     * @param   key                 计算属性名称
+     * @param   exec                  计算属性计算逻辑
+     * @param   onChange            计算计算属性变化触发的函数
+     */
+    constructor(obj, key, exec, onChange) {
+        this.obj = obj
+        this.key = key
+        this.exec = exec
+        this.onChange = onChange
+        return this.defineComputed()
+    }
+
+    defineComputed() {
+        const self = this
+        const onDepUpdated = () => {
+            const val = self.exec()
+            this.onChange(val)
+        }
+
+        Object.defineProperty(self.obj, self.key, {
+            get() {
+                Dep.target = onDepUpdated
+                const val = self.exec()
+                Dep.target = null
+                return val
+            },
+            set() {
+                console.error('计算属性无法被赋值！')
+            }
+        })
+    }
+}
+
+const hero = new Observable({
+    health: 3000,
+    IQ: 150
+})
+
+new Compute(hero,
+    'type',
+    () => {
+        console.log('get type')
+        return hero.health > 4000 ? '坦克' : '脆皮'
+    },
+    (val) => {
+        console.log(`我的类型是：${val}`)
+    })
+
+console.log(`英雄初始类型：${hero.type}`)
+console.log(`英雄初始类型：${hero.type}`)
+hero.health = 5000
+
+
 export default class App extends React.Component {
+
+    data() {
+        return {
+            a: 111,
+            b: 222,
+            c: 333,
+            d: 444,
+        }
+    }
 
     constructor(props) {
         super(props)
+
+        const data = this.data()
+
+
         this.state = {
-            a: '111',
-            b: '222',
-            c: '333',
-            d: '444',
+            data,
         }
     }
 
     componentDidMount() {
-        console.log('mounted')
-    }
-
-    componentWillUnmount() {
-        console.log('destroyed')
-    }
-
-    get ab() {
-        console.log('ab')
-        return this.state.a + '' + this.state.b
-    }
-
-    get cd() {
-        console.log('cd')
-        return this.state.c + '' + this.state.d
+        console.log(this.state.data)
     }
 
     render() {
@@ -44,7 +147,6 @@ export default class App extends React.Component {
                     <div className="app-right">
                         <input type="text" value={this.state.a} onChange={e => this.pl_change(e, 'a')}/>
                         <input type="text" value={this.state.b} onChange={e => this.pl_change(e, 'b')}/>
-                        ab:[{this.ab}]----cd:[{this.cd}]
                     </div>
                 </div>
             </div>
